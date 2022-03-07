ad97f802f757a4ead993d3aa256f5576752584ac
+ <+>
2022-03-07 16:29:44 -0500

    commit tree 5092f6add38af11f3dd4bf75750292d9a0b28194

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => title.split(' '))
+      .map(
+        wordArray => wordArray.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ')
+  )
 }
